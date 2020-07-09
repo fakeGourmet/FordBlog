f024271255f1a7579f35a28730248297c5be779a
zhoufeng.ford <zhoufeng.ford@bytedance.com>
2020-07-09 11:18:49 +0800

    add wwdc doc and remove ignored files
    Change-Id: I3e6224a446d84936d5977de22e50bc1a748ef247

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,14 +1,18 @@
 module.exports = {
-  theme: 'vuepress-theme-reco',
+  // host: '127.0.0.1'
+  port: '233',
+  title: '一个兴趣使然的博客',
   head: [
     [
       'meta',
       {
         name: 'viewport',
-        content: 'width=device-width,initial-scale=1,user-scalable=no' 
-      }
-    ]
+        content: 'width=device-width,initial-scale=1,user-scalable=no'
+      },
+    ],
   ],
+  // 主题配置
+  theme: 'vuepress-theme-reco',
   themeConfig: {
     type: 'blog',
     author: 'Fake Gourmet',
@@ -23,10 +27,18 @@ module.exports = {
         text: 'TimeLine',
         link: '/timeline/',
         icon: 'reco-date'
-      }
+      },
+      {
+        text: 'Contact',
+        icon: 'reco-message',
+        items: [
+          { text: 'GitHub', link: 'https://github.com/fakeGourmet', icon: 'reco-github' }
+        ],
+      },
     ],
     sidebar: 'auto',
-     // 博客配置
+    startYear: '2020',
+    // 博客配置
     blogConfig: {
       category: {
         location: 2,     // 在导航栏菜单中所占的位置，默认2
@@ -35,8 +47,38 @@ module.exports = {
       tag: {
         location: 3,     // 在导航栏菜单中所占的位置，默认3
         text: 'Tag'      // 默认文案 “标签”
-      }
+      },
     },
     lastUpdated: 'Last Updated'
-  }
+  },
+  markdown: {
+      lineNumbers: true //代码显示行号
+  },
+  // 搜索设置
+  search: true,
+  searchMaxSuggestions: 10,
+  // 插件
+  plugins: [
+    ['flowchart'],  // 支持流程图
+    [
+      '@vuepress-reco/vuepress-plugin-bgm-player',
+      {
+        autoShrink: true,
+        floatPosition: 'right',
+        audios: [
+          {
+            name: '강남역 4번 출구',
+            artist: 'Plastic / Fallin` Dild',
+            url: 'https://assets.smallsunnyfox.com/music/2.mp3',
+            cover: 'https://assets.smallsunnyfox.com/music/2.jpg'
+          },
+        ],
+      },
+    ],
+    ['@vuepress-reco/vuepress-plugin-kan-ban-niang'],
+    ['@vuepress-reco/vuepress-plugin-screenfull'],
+    ['vuepress-plugin-smooth-scroll'],  // 平滑滚动
+    ['@vuepress/nprogress'],  // 加载进度条
+    ['reading-progress']  // 阅读进度条
+  ]
 }
